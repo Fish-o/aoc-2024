eb219111dab9981c9f6c801ff1f32c2f6d0e5f9c
Conner Hof <c.l.hof@student.utwente.nl>
2024-12-01 06:52:42 +0100

    Day 01 🎉

---
+++ src/bin/01.rs
@@ -0,0 +1,72 @@
+use advent_of_code::utils::*;
+use itertools::Itertools;
+use num::Integer;
+use rayon::prelude::*;
+use std::{
+    collections::HashMap,
+    f32::consts::E,
+    fmt::{Debug, Display},
+};
+advent_of_code::solution!(1);
+
+pub fn part_one(input: &str) -> Option<usize> {
+    let mut a = input
+        .trim()
+        .lines()
+        .map(|e| e.split_once("   ").unwrap())
+        .map(|(a, b)| (a.parse::<usize>().unwrap(), b.parse::<usize>().unwrap()))
+        .fold((vec![], vec![]), |mut a, b| {
+            a.0.push(b.0);
+            a.1.push(b.1);
+            (a.0, a.1)
+        });
+
+    a.0.sort();
+    a.1.sort();
+
+    Some(a.0.iter().zip(a.1).map(|(a, b)| a.abs_diff(b)).sum())
+}
+
+pub fn part_two(input: &str) -> Option<usize> {
+    let mut a = input
+        .trim()
+        .lines()
+        .map(|e| e.split_once("   ").unwrap())
+        .map(|(a, b)| (a.parse::<usize>().unwrap(), b.parse::<usize>().unwrap()))
+        .fold((vec![], vec![]), |mut a, b| {
+            a.0.push(b.0);
+            a.1.push(b.1);
+            (a.0, a.1)
+        });
+
+    a.1.sort();
+    let h: HashMap<usize, usize> = HashMap::new();
+    let (v, c, mut h) = a.1.iter().fold((0, 0, h), |(x, mut c, mut h), v| {
+        if &x == v {
+            c += 1;
+        } else {
+            h.insert(x, c);
+            c = 1;
+        }
+        (*v, c, h)
+    });
+    h.insert(v, c);
+    Some(a.0.iter().map(|v| v * h.get(v).or(Some(&0)).unwrap()).sum())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(11));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(31));
+    }
+}
